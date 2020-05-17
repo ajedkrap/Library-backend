01457156d4d7774545be3872b7500a7a5d382266
Dezza Rizqi <mr.thedja@gmail.com>
2020-05-18 03:36:40 +0700

    delete unused method

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,7 +2,5 @@ const route = require('express').Router()
 const userController = require('../controllers/users')
 
 route.get('/', userController.getAllUsers)
-route.patch('/:id', userController.updateUser)
-route.delete('/:id', userController.deleteUser)
 
 module.exports = route
